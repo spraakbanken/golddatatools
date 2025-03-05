9e361886671125597254c62b279f1fefd26a9762
Aleksandrs Berdicevskis <aleksandrs.berdicevskis@gu.se>
2025-03-05 16:17:46 +0100

    participles

---
+++ extract_participles.rb
@@ -0,0 +1,16 @@
+inputfile = File.open("eukalyptus_all.conllu","r:utf-8")
+
+partpenult = "abcdfghjklmnpqrstvwxz"
+lemmas = {}
+inputfile.each_line do |line|
+    line1 = line.strip
+    if line1 != "" and line1[0] != "#"
+        line2 = line1.split("\t")
+        if line2[3] == "AJ" and ((line2[2][-1] == "d" and partpenult.include?(line2[2][-2])) or (line2[2][-2..-1] == "en"))
+            lemmas[line2[2]] = true
+        end
+    end
+
+end
+
+STDOUT.puts lemmas.keys
